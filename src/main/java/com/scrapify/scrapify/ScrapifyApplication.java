872ce77e232868234cbe3b17236fb613a7a3d15b
Mounish G <mounishganesh2000@gmail.com>
2023-03-28 11:02:49 +0530

    initial commit

---
+++ src/main/java/com/scrapify/scrapify/ScrapifyApplication.java
@@ -0,0 +1,13 @@
+package com.scrapify.scrapify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrapifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrapifyApplication.class, args);
+	}
+
+}
