955ce7533abfdca5a2324bfdd80481594f7240ad
Mounish G <mounishganesh2000@gmail.com>
2023-04-04 14:00:00 +0530

    Swagger configuration completed

--- src/main/java/com/scrapify/scrapify/repository/ProductRegionMapRepository.java
+++ src/main/java/com/scrapify/scrapify/repository/ProductRegionMapRepository.java
@@ -1,14 +1,16 @@
 package com.scrapify.scrapify.repository;
 
 import com.scrapify.scrapify.entity.ProductRegionMap;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ProductRegionMapRepository extends JpaRepository<ProductRegionMap,String> {
 
-    List<ProductRegionMap> findByRegion(@Param("region") String id);
+    @EntityGraph(attributePaths = {"product","region"})
+    List<ProductRegionMap> findAllByRegionId(String id);
 
-    List<ProductRegionMap> findByProduct(@Param("product") String id);
+    @EntityGraph(attributePaths = {"product","region"})
+    List<ProductRegionMap> findAllByProductId(String id);
 }
